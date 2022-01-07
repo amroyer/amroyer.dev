eb7a8886a0a4e2046bd35dc6fbf7870e08865bac
Aaron Royer <amroyerdev@gmail.com>
2022-01-07 11:47:52 -0700

    Added social icons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
-<div class="grid place-items-center w-full min-h-screen bg-slate-50">
-	<h1 class="text-5xl text-slate-900">COMING SOON</h1>
+<div class="flex flex-col w-full min-h-screen">
+	<h1 class="text-5xl m-auto">COMING SOON</h1>
+	<footer class="flex mx-auto gap-1 mb-1">
+		<a href="https://twitter.com/amroyerdev"><i class="fab fa-twitter-square text-3xl" /></a>
+		<a href="https://github.com/amroyer"><i class="fab fa-github-square text-3xl" /></a>
+	</footer>
 </div>
