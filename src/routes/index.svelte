fd7d097c8fbbc168c802d68c72f41746b8a9a7d8
Aaron Royer <amroyerdev@gmail.com>
2022-03-11 07:55:15 -0700

    Change landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <div class="flex flex-col w-full min-h-screen">
-	<h1 class="text-5xl m-auto">COMING SOON</h1>
+	<h1 class="text-5xl m-auto capitalize">Aaron M Royer</h1>
+	<h2 class="text-3xl m-auto capitalize">Development, Data, and Democracy</h2>
 	<footer class="flex mx-auto gap-1 mb-1">
 		<a href="https://twitter.com/amroyerdev"><i class="fab fa-twitter-square text-3xl" /></a>
 		<a href="https://github.com/amroyer"><i class="fab fa-github-square text-3xl" /></a>
