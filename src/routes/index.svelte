485a97fc3cd313dd44387a0b5f3ee9e0876a30e5
Aaron Royer <amroyerdev@gmail.com>
2022-03-11 07:58:59 -0700

    Fix formatting on landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,8 @@
 <div class="flex flex-col w-full min-h-screen">
-	<h1 class="text-5xl m-auto capitalize">Aaron M Royer</h1>
-	<h2 class="text-3xl m-auto capitalize">Development, Data, and Democracy</h2>
+	<div class="m-auto">
+		<h1 class="text-5xl mx-auto capitalize">Aaron M Royer</h1>
+		<h2 class="text-3xl mx-auto capitalize">Development, Data, and Democracy</h2>
+	</div>
 	<footer class="flex mx-auto gap-1 mb-1">
 		<a href="https://twitter.com/amroyerdev"><i class="fab fa-twitter-square text-3xl" /></a>
 		<a href="https://github.com/amroyer"><i class="fab fa-github-square text-3xl" /></a>
